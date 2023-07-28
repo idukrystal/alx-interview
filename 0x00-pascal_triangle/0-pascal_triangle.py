654903a283ed4b9c57a3f83929c4e17fcb5e1d15
Crysart <idukrystal@gmail.com>
2023-07-28 02:29:08 +0100

    first attempt6

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -18,15 +18,12 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    # Initialize the triangle with the first row containing 1.
     triangle = [[1]]
 
-    # Generate the remaining rows of the triangle.
     while len(triangle) < n:
         last_row = triangle[-1]
         next_row = [1]
 
-        # Calculate the values for the next row
         for i in range(1, len(last_row)):
             next_row.append(last_row[i - 1] + last_row[i])
 
