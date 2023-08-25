898d7764c088d9ee5060e1b90540dc2a56a6b9e5
Crysart <idukrystal@gmail.com>
2023-08-25 13:42:32 +0100

    t21

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 """  Write a method that determines if a
 given data set represents a valid UTF-8 encoding.
@@ -17,7 +16,7 @@ def validUTF8(data):
                 rem_bytes = 0
             elif byte >> 5 == 0b110:
                 rem_bytes = 1
-            elif byte  >> 4 == 0b1110:
+            elif byte >> 4 == 0b1110:
                 rem_bytes = 2
             elif byte >> 3 == 0b11110:
                 rem_bytes = 3
@@ -28,7 +27,3 @@ def validUTF8(data):
                 return False
             rem_bytes -= 1
     return rem_bytes == 0
-
-#data = [0xF4, 0x8F, 0xBF, 0xBD, 0xF0, 0x90, 0x80, 0x80] 
-#print(validUTF8(data))
-# print(x >> 7)
