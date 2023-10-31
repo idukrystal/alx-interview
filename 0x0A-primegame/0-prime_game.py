bc431decedd2f8de1d5642d6406a64475fa96882
Crysart <idukrystal@gmail.com>
2023-10-31 15:16:05 +0100

    done

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+def isWinner(x, nums):
+    players = {
+        "Maria": 0,
+        "Ben": 0
+    }
+    i = 0
+    while i < x:
+        clist = list(range(1, nums[i]+1))
+        player = None
+        while True:
+            player = "Ben" if player == "Maria" else "Maria"
+            found = False
+            for num in clist:
+                if isPrime(num):
+                    found = True
+                    clist = removeMultiples(num, clist)
+                    break
+            if not found:
+                winner = "Ben" if player == "Maria" else "Maria"
+                players[winner] += 1
+                break
+        i += 1
+        if players["Maria"] < players["Ben"]:
+            return "Ben"
+        elif players["Maria"] == players["Ben"]:
+            return "Tie"
+        else:
+            return "Maria"
+
+
+def isPrime(num):
+    if (num == 2):
+        return True
+    if ((num % 2) == 0 or num == 1):
+        return False
+    if (num == 3):
+        return True
+    for i in range(2, (num//2), 2):
+        if not (((num % i)) == 0):
+            return False
+    return True
+
+
+def removeMultiples(num, clist):
+    multiples = []
+    for i in clist:
+        if (i % num) == 0:
+            multiples.append(i)
+    clist = list(filter(lambda x: x not in multiples, clist))
+    return clist
---
+++ 0x0A-primegame/README.md
@@ -0,0 +1,1 @@
+an alx interview project in python
